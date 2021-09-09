5719e72cac42e9a47bbc2debd78a6d0a6ceaa9d5
abi-hong <flffjakfwm20@gmail.com>
2021-09-09 22:46:50 +0900

    first & add header, carousel

---
+++ src/Pages/MainPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Components/Header";
+import ImageMenu from "../Components/ImageMenu";
+
+function MainPage() {
+  return (
+    <div>
+      <Header />
+      <ImageMenu />
+    </div>
+  );
+}
+
+export default MainPage;
