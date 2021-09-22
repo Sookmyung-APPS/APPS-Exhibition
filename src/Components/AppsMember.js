e7627bb85c8ebb0d32b58cd13752ae4cace6c013
OliviaYJH <0703olivia@naver.com>
2021-09-22 21:03:16 +0900

    Revert "update: AppsMembers"
    This reverts commit eccacc56c8d3bc63de520dee3e50a126991456b2.

--- src/Components/AppsMember.js
+++ src/Components/AppsMember.js
@@ -1,96 +1,53 @@
 import React from "react";
-//import '../App.css';
-
 
 
 function AppsMember(){
-
     return(
         <div class="container">
-            <h1 class="ImgTitle"> 2021 Sookmyung APPS Member</h1>
+            <h1> 2021 Sookmyung APPS Member</h1>
             <table class="table">
                 <tbody>
                     <tr>
-                        <td><img src={require("../Assets/MembersImg/Chaeyeong(심채영).jpg").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Chaeyoung(정채영).png").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Dabin(윤다빈).jpg").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Dani(강단이).jpg").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Dayeon(김다연).PNG").default} width="120"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
                     </tr>
                     <tr>
-                        <td>Chaeyeong</td>
-                        <td>Chaeyoung</td>
-                        <td>Dabin</td>
-                        <td>Dani</td>
-                        <td>Dayeon</td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
                     </tr>
                     <tr>
-                        <td><img src={require("../Assets/MembersImg/Eunhyung(최은형).png").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Gaeul(이가을).jpg").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Haeji(박해지).png").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Hyeonae(장현애).png").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Hyesu(김혜수).png").default} width="120"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
                     </tr>
                     <tr>
-                        <td>Eunhyung</td>
-                        <td>Gaeul</td>
-                        <td>Haeji</td>
-                        <td>Hyeonae</td>
-                        <td>Hyesu</td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
                     </tr>
                     <tr>
-                        <td><img src={require("../Assets/MembersImg/Jane(정제인).png").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Jeonghyeon(황정현).jpeg").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Jeongyeon(서정연).jpeg").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Jinhee(박진희).jpg").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Jinyoung(김진영).png").default} width="120"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
                     </tr>
                     <tr>
-                        <td>Jane</td>
-                        <td>Jeonghyeon</td>
-                        <td>Jeonghyeon</td>
-                        <td>Jinhee</td>
-                        <td>Jinyoung</td>
-                    </tr>
-                    <tr>
-                        <td><img src={require("../Assets/MembersImg/Jinyoung(신진영).png").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Junghyun(유정현).jpg").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Minji(권민지).png").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Minyeong(정민영).png").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Nari(장나리).png").default} width="120"></img></td>
-                    </tr>
-                    <tr>
-                        <td>Jinyoung</td>
-                        <td>Junghyun Yu</td>
-                        <td>Minji</td>
-                        <td>Minyeong</td>
-                        <td>Nari</td>
-                    </tr>
-                    <tr>
-                        <td><img src={require("../Assets/MembersImg/SuJin(조수진).jpg").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Suyeon(황수연).png").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Yebin(최예빈).png").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Yejin(김예진).jpeg").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Yerin(고예린).png").default} width="120"></img></td>
-                    </tr>
-                    <tr>
-                        <td>SuJin</td>
-                        <td>Suyeon</td>
-                        <td>Yebin</td>
-                        <td>Yejin</td>
-                        <td>Yerin</td>
-                    </tr>
-                    <tr>
-                        <td><img src={require("../Assets/MembersImg/Yoojeong(전유정).png").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Youngseo(조영서).png").default} width="120"></img></td>
-                        <td><img src={require("../Assets/MembersImg/Yugyeong(현유경).png").default} width="100"></img></td>
-                        <td><img src={require("../Assets/MembersImg/YuKyung Hwang(황유경).jpg").default} width="120"></img></td>
-                    </tr>
-                    <tr>
-                        <td>Yoojeong</td>
-                        <td>Youngseo</td>
-                        <td>Yugyeong</td>
-                        <td>YuKyung Hwang</td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
+                        <td><img src={require("../Assets/apps_logo.png").default} width="100"></img></td>
                     </tr>
                 </tbody>
             </table>
