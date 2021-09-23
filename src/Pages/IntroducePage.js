c77614472f71f09b5f7936b13c8f5e96568c518d
OliviaYJH <0703olivia@naver.com>
2021-09-23 20:14:26 +0900

    Add: css

--- src/Pages/IntroducePage.js
+++ src/Pages/IntroducePage.js
@@ -2,17 +2,18 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import AppsInfo from "../Components/AppsInfo";
 import AppsMember from "../Components/AppsMember";
+import '../Assets/IntroducePage.css'
 
 
 function IntroduePage(){
     const infoimg = "../Assets/apps_logo.png";
    return(
        <div class="container">
-
+           <div id="BigAppsImg">
            <img
             src={require("../Assets/apps_logo.png").default}
-            className="w-100"
-          />
+            className="w-75"
+          /></div>
           <AppsInfo/>
           <AppsMember/>
 
