dfd87a6603c31f6c36f5a81b2a9f54cc08d67208
OliviaYJH <0703olivia@naver.com>
2021-09-26 22:45:54 +0900

    add: introducepage header & change mimoji

--- src/Pages/IntroducePage.js
+++ src/Pages/IntroducePage.js
@@ -3,12 +3,14 @@ import { Container } from "react-bootstrap";
 import AppsInfo from "../Components/AppsInfo";
 import AppsMember from "../Components/AppsMember";
 import '../Assets/IntroducePage.css'
+import Header from "../Components/Header";
 
 
 function IntroduePage(){
     const infoimg = "../Assets/apps_logo.png";
    return(
        <div class="container">
+           <Header />
            <div id="BigAppsImg">
            <img
             src={require("../Assets/apps_logo.png").default}
