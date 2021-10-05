646ed7a7976b947f311efdcacc636d2ed67e3e5c
abi-hong <68041042+abi-hong@users.noreply.github.com>
2021-10-06 00:24:34 +0900

    video

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,17 +1,11 @@
 import React from "react";
+import "../Assets/videoStyle.css";
 
 function Video() {
-  const iframeStyle = {
-    width: "900px",
-    height: "450px",
-    margin: "0 auto",
-  };
-
   return (
     <div class="boxWarp" style={{ display: "flex", height: "100%" }}>
       <iframe
         class="box"
-        style={iframeStyle}
         src="https://www.youtube.com/embed/yCvSR4lSqTg"
         title="YouTube video player"
         frameborder="0"
