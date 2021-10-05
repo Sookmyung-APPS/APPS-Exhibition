3308274614d21920c0450639ab7e35f8c0cb7084
뽁 <o0o1910419@sookmyung.ac.kr>
2021-10-05 17:10:19 +0900

    add git/googleDriveLink Complete
    전부 추가 완료

--- src/Components/Download.js
+++ src/Components/Download.js
@@ -3,20 +3,20 @@ import React from "react";
 const array = [
     {
         name: "스팟지도웹서비스",
-        DownloadLink1: "https://github.com/jung-jane",
-        DownloadLink2: "https://github.com/jung-jane",
-        GithubLink: "https://github.com/jung-jane"    
+        DownloadLink1: "#",
+        DownloadLink2: "#",
+        GithubLink: "https://github.com/Sookmyung-APPS/Captured-Moment"    
     },
     {
         name: "DeliveryRun",
-        DownloadLink1: "https://github.com/jung-jane",
-        DownloadLink2: "https://www.naver.com/",
+        DownloadLink1: "https://drive.google.com/file/d/1-QBDGhRGq4kwByqPQFYFjXj4X-fwdPgx/view?usp=sharing",
+        DownloadLink2: "https://drive.google.com/file/d/1-QBDGhRGq4kwByqPQFYFjXj4X-fwdPgx/view?usp=sharing",
         GithubLink: "https://github.com/Sookmyung-APPS/DeliveryRun"    
     },
     {
         name: "일력",
-        DownloadLink1: "https://github.com/jung-jane",
-        DownloadLink2: "https://www.naver.com/",
+        DownloadLink1: "#",
+        DownloadLink2: "#",
         GithubLink: "https://github.com/Sookmyung-APPS/Dia-lendar"    
     },
     {
@@ -27,20 +27,20 @@ const array = [
     },
     {
         name: "SoundOfFairies",
-        DownloadLink1: "",
+        DownloadLink1: "https://drive.google.com/file/d/1ETk4Bk264G4NTTukfPhsnjkq2Faubtbt/view?usp=sharing",
         DownloadLink2: "https://drive.google.com/file/d/1ETk4Bk264G4NTTukfPhsnjkq2Faubtbt/view?usp=sharing",
-        GithubLink: "https://github.com/jung-jane"    
+        GithubLink: "https://github.com/Sookmyung-APPS/SoundOfFairies"    
     },
     {
         name: "Comsongz",
-        DownloadLink1: "https://github.com/jung-jane",
-        DownloadLink2: "https://www.naver.com/",
-        GithubLink: "https://github.com/jung-jane"    
+        DownloadLink1: "#",
+        DownloadLink2: "#",
+        GithubLink: "https://github.com/Sookmyung-APPS/Comsongz"    
     },
     {
         name: "ALittleDruid",
-        DownloadLink1: "https://github.com/jung-jane",
-        DownloadLink2: "https://www.naver.com/",
+        DownloadLink1: "https://drive.google.com/file/d/1Gj8KuM6f8XTBbgoogaUnAA-fTwnCMjF6/view?usp=sharing",
+        DownloadLink2: "https://drive.google.com/file/d/1J-xFZYxq7J6TcnW-ehJaLcV2aZh9YE6y/view?usp=sharing",
         GithubLink: "https://github.com/Sookmyung-APPS/Legend-Of-A-Little-Druid"    
     },
     {   //강단이
@@ -52,21 +52,21 @@ const array = [
     },
     {   //김다연
         name: "김다연",
-        DownloadLink1: "https://github.com/jung-jane",
-        DownloadLink2: "https://www.naver.com/",
+        DownloadLink1: "https://drive.google.com/file/d/1K-R09VSp8ssAEaY0n72uw7kAgO4ylSuP/view?usp=sharing",
+        DownloadLink2: "https://drive.google.com/file/d/1K-R09VSp8ssAEaY0n72uw7kAgO4ylSuP/view?usp=sharing",
         GithubLink: "https://github.com/Sookmyung-APPS/TERRAFORMING"
     },
     {   //김진영
         name: "김진영",
-        DownloadLink1: "https://github.com/jung-jane",
-        DownloadLink2: "https://www.naver.com/",
+        DownloadLink1: "https://drive.google.com/file/d/1F2OrFcJeqqiWIAaAmuL711jp30GcRLzh/view?usp=sharing",
+        DownloadLink2: "https://drive.google.com/file/d/1F2OrFcJeqqiWIAaAmuL711jp30GcRLzh/view?usp=sharing",
         GithubLink: "https://github.com/Sookmyung-APPS/Make-My-Wish-Come-True"
     },
     {   //박해지
         name: "박해지",
-        DownloadLink1: "https://github.com/jung-jane",
-        DownloadLink2: "https://www.naver.com/",
-        GithubLink: "https://github.com/jung-jane"
+        DownloadLink1: "https://drive.google.com/file/d/1jq1tFL5M-F8wJBISxmXgY0Qvq1uCjKzJ/view?usp=sharing",
+        DownloadLink2: "https://drive.google.com/file/d/1jq1tFL5M-F8wJBISxmXgY0Qvq1uCjKzJ/view?usp=sharing",
+        GithubLink: "https://github.com/Sookmyung-APPS/RoomEscapeGame"
     },
     {   //이가을
         name: "이가을",
@@ -76,27 +76,27 @@ const array = [
     },
     {   //장나리
         name: "장나리",
-        DownloadLink1: "https://github.com/jung-jane",
-        DownloadLink2: "https://www.naver.com/",
+        DownloadLink1: "https://drive.google.com/file/d/1b9qbpwHg6J8nC_aKBz4t7WgqJZdWauVw/view?usp=sharing",
+        DownloadLink2: "https://drive.google.com/file/d/1b23IXQ023QW9Njn39opxA67ZrRo7TO9v/view?usp=sharing",
         GithubLink: "https://github.com/Sookmyung-APPS/SNOWRUN"
     },
     {   //정민영
         name: "정민영",
-        DownloadLink1: "https://github.com/jung-jane",
-        DownloadLink2: "https://www.naver.com/",
+        DownloadLink1: "https://drive.google.com/file/d/1JXHYKX225fo9zcREasEIvSTOVcpoBfod/view?usp=sharing",
+        DownloadLink2: "https://drive.google.com/file/d/15teX9O5Zfo9MrRpE3OxJr7QcF8uUC1tB/view?usp=sharing",
         GithubLink: "https://github.com/Sookmyung-APPS/2DSnowflakeGame"
     },
     {   //정채영
         name: "정채영",
-        DownloadLink1: "https://github.com/jung-jane",
-        DownloadLink2: "https://www.naver.com/",
-        GithubLink: "https://github.com/jung-jane"
+        DownloadLink1: "#",
+        DownloadLink2: "#",
+        GithubLink: "https://github.com/Sookmyung-APPS/RollingBall"
     },
     {   //홍희수
         name: "홍희수",
-        DownloadLink1: "https://github.com/jung-jane",
-        DownloadLink2: "https://www.naver.com/",
-        GithubLink: "https://github.com/jung-jane"
+        DownloadLink1: "https://drive.google.com/file/d/1M-WuAsOH1HxPO_cpUoOa9I8Mt2T-RoPf/view?usp=sharing",
+        DownloadLink2: "https://drive.google.com/file/d/1M-WuAsOH1HxPO_cpUoOa9I8Mt2T-RoPf/view?usp=sharing",
+        GithubLink: "https://github.com/Sookmyung-APPS/RollNoonSong"
     },
 ];
 
