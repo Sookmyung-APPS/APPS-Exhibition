3c3c6416a91de3965fd2dbbb421776ccb7822dc4
ilmerry <milkiss0414@naver.com>
2021-10-06 00:26:24 +0900

    background image

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 import ImageMenu from "../Components/ImageMenu";
 import Video from "../Components/Video";
 import ImagePoster from "../Components/ImagePoster";
@@ -233,6 +234,7 @@ function MainPage() {
             </tbody>
           </table>
         </div>
+      <Footer />
       </div>
     </div>
   );
