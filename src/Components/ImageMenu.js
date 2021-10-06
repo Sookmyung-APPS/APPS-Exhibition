c8a09c8d1cb8ebd1df1c217ada7a05eb5b71cad0
abi-hong <68041042+abi-hong@users.noreply.github.com>
2021-10-06 21:12:11 +0900

    change carousel

--- src/Components/ImageMenu.js
+++ src/Components/ImageMenu.js
@@ -1,23 +1,16 @@
 import { Carousel } from "react-bootstrap";
 import React from "react";
-import "../Assets/main.css"
 
 function ImageMenu() {
   return (
-    <Carousel className="CarouselStyle">
+    <Carousel>
       <Carousel.Item>
         <img
           className="d-block w-100"
           src={require("../Assets/mainpage_carousel.png").default}
           alt="First slide"
         />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={require("../Assets/mainpage_carousel.png").default}
-          alt="Second slide"
-        />
+        <Carousel.Caption />
       </Carousel.Item>
     </Carousel>
   );
